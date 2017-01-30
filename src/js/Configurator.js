d77b9f3df1efacc7c0fa99e447750b332f39540e
Bartosz Mediger <bartosz.mediger@withpassion.pl>
2017-01-30 14:08:36 +0100

    Transport + Dodatki

--- src/js/Configurator.js
+++ src/js/Configurator.js
@@ -10,20 +10,22 @@
 	Configurator.prototype.enable = function() {
 		var next = document.getElementById('configuratorNext'),
 			navbar = document.getElementById('navBar'),
-			steps = document.getElementById('configuratorSteps');
+			steps = document.getElementById('configuratorSteps'),
+			stepsNum = $('> li', steps).length,
+			i = 0;
 
 		next.addEventListener("click", function(e) {
 			e.preventDefault();
-			
-			$('> li', steps).addClass('is-hidden');
-			$('> li', steps).next('li').removeClass('is-hidden').addClass('is-active');
-			
-			$('li', navbar).eq( $('> li.is-active', steps).index() ).addClass('is-active');
-			
-			waterdreamer.helper.goToTarget('#configurator');
-			
-			if ( $('> li.is-active', steps).index() == $(steps).length ) {
-				$(next).removeClass('bg-navy').addClass('bg-gray--darker');
+
+			if (i < 3) {
+				i ++;
+	
+				$('li', navbar).eq( i ).addClass('is-active');
+				
+				$('> li', steps).addClass('is-hidden');
+				$('> li', steps).eq(i).removeClass('is-hidden').addClass('is-active');
+				
+				waterdreamer.helper.goToTarget('#configurator');
 			}
 		});
 	};
